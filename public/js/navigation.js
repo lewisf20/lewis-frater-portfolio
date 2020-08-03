65c4128b9c72d48f807cda50f92f9dcff0df0188
lewisf20 <lewis_frater@yahoo.com>
2020-08-03 13:24:31 +0100

    Mobile nav menu implemented - bug on projects.html

---
+++ public/js/navigation.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.nav__hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuList = document.querySelector('.mobile-menu__list');
+const mobileLogo = document.querySelector('.mobile-menu__logo');
+
+// On menu click -- apply css classes to elements
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('nav__hamburger--close');
+  mobileMenu.classList.toggle('mobile-menu--active');
+  mobileMenuList.classList.toggle('mobile-menu__list--active');
+  mobileLogo.classList.toggle('mobile-menu__logo--active');
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,51 @@
-let indexChittr = 0;
-let indexLearn = 0;
-let indexEport = 0;
+function init() {
+  let indexChittr = 0;
+  let indexLearn = 0;
+  let indexEport = 0;
 
-imageChange();
-
-function imageChange() {
   //Get images via their class
   const chittrImages = document.querySelectorAll('.chittrImage');
   const learningImages = document.querySelectorAll('.learningImage');
   const eportfolioImages = document.querySelectorAll('.eportfolioImage');
 
+  imageChange();
+
+  function imageChange() {
+    //Set display to none for each image
+    eachImage(chittrImages);
+    indexChittr++;
+
+    eachImage(learningImages);
+    indexLearn++;
+
+    eachImage(eportfolioImages);
+    indexEport++;
+
+    if (indexChittr > chittrImages.length) {
+      indexChittr = 1;
+    }
+    if (indexLearn > learningImages.length) {
+      indexLearn = 1;
+    }
+    if (indexEport > eportfolioImages.length) {
+      indexEport = 1;
+    }
+
+    //Set one image to display block
+    chittrImages[indexChittr - 1].style.display = 'block';
+    learningImages[indexLearn - 1].style.display = 'block';
+    eportfolioImages[indexEport - 1].style.display = 'block';
+
+    //call itself every 3.5 seconds
+    setTimeout(imageChange, 3500);
+  }
+
   //Set display to none for each image
-  eachImage(chittrImages);
-  indexChittr++;
-
-  eachImage(learningImages);
-  indexLearn++;
-
-  eachImage(eportfolioImages);
-  indexEport++;
-
-  if (indexChittr > chittrImages.length) {
-    indexChittr = 1;
+  function eachImage(imageArray) {
+    imageArray.forEach((element) => {
+      element.style.display = 'none';
+    });
   }
-  if (indexLearn > learningImages.length) {
-    indexLearn = 1;
-  }
-  if (indexEport > eportfolioImages.length) {
-    indexEport = 1;
-  }
-
-  //Set one image to display block
-  chittrImages[indexChittr - 1].style.display = 'block';
-  learningImages[indexLearn - 1].style.display = 'block';
-  eportfolioImages[indexEport - 1].style.display = 'block';
-
-  //call itself every 3.5 seconds
-  setTimeout(imageChange, 3500);
 }
 
-//Set display to none for each image
-function eachImage(imageArray) {
-  imageArray.forEach((element) => {
-    element.style.display = 'none';
-  });
-}
+init();
